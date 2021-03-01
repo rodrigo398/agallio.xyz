37535794f2b0cfe169891edf420b7aa8e5df2da7
agallio <agalliosamai@gmail.com>
2021-03-01 17:13:18 +0700

    Update packages version & handle 404 in different lang

---
+++ src/locales/404.js
@@ -0,0 +1,6 @@
+const notFoundLocale = {
+  text: { id: '404 - Halaman Tidak Ditemukan!', en: '404 - Not Found!' },
+  button: { id: 'Kembali ke Beranda', en: 'Return Home' },
+}
+
+export default notFoundLocale
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,20 @@
+import { useRouter } from 'next/router'
 import Link from 'next/link'
 
+import notFoundLocale from '../locales/404'
+
 export default function NotFound() {
+  const router = useRouter()
+  const { locale } = router
+
   return (
     <div className="flex flex-col pt-32 items-center justify-center">
-      <h1 className="mb-4 text-3xl tracking-wide font-black md:text-4xl dark:text-white">
-        404 - Not Found
+      <h1 className="mb-4 text-3xl tracking-wide font-black md:text-3xl dark:text-white">
+        {notFoundLocale.text[locale]}
       </h1>
       <Link href="/">
-        <a className="w-40 py-2 border border-green-600 sm:w-40 rounded-lg text-green-600 transition transform text-center hover:text-white hover:border-green-600 hover:bg-green-600 dark:text-green-200 dark:border-green-200 dark:hover:bg-green-900">
-          Return Home
+        <a className="py-2 px-4 border border-green-600 rounded-lg text-green-600 transition transform text-center hover:text-white hover:border-green-600 hover:bg-green-600 dark:text-green-200 dark:border-green-200 dark:hover:bg-green-900">
+          {notFoundLocale.button[locale]}
         </a>
       </Link>
     </div>
